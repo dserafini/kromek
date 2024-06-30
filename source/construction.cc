e1735f25e18459b001c91bef1b0de30539ab8517
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2024-06-30 13:48:44 +0200

    Update construction.cc

--- source/construction.cc
+++ source/construction.cc
@@ -19,7 +19,7 @@ void MyDetectorConstruction::DefineMaterials()
 	G4cout << "MyDetectorConstruction::DefineMaterials" << G4endl;
 	G4NistManager *nist = G4NistManager::Instance();
 
-	G4Material* materialAir      = nist->FindOrBuildMaterial("G4_AIR");
+	G4Material*      = nist->FindOrBuildMaterial("G4_AIR");
 }
 
 
@@ -29,10 +29,10 @@ G4VPhysicalVolume* MyDetectorConstruction::Construct()
 	G4double world_half_Z  = 10*cm;
 	G4double world_half_XY = 10*cm;
 
-	solidWorld = new G4Box("solidWorld", world_half_XY, world_half_XY, world_half_Z);
-	logicWorld = new G4LogicalVolume(solidWorld, materialAir, "logicWorld", 0, 0, 0, true);
+	G4Box* solidWorld = new G4Box("solidWorld", world_half_XY, world_half_XY, world_half_Z);
+	G4LogicalVolume* logicWorld = new G4LogicalVolume(solidWorld, materialAir, "logicWorld", 0, 0, 0, true);
 
-	physWorld = new G4PVPlacement(0, G4ThreeVector(), logicWorld, "physWorld", 0, false, 0, true);
+	G4VPhysicalVolume* physWorld = new G4PVPlacement(0, G4ThreeVector(), logicWorld, "physWorld", 0, false, 0, true);
 
 	return physWorld;
 }
