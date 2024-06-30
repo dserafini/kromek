cfbba340e2886aa872dbe9013e6c491caa19d29c
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2024-06-30 20:38:54 +0200

    Update generator.cc

--- source/generator.cc
+++ source/generator.cc
@@ -4,6 +4,14 @@ MyPrimaryGenerator::MyPrimaryGenerator()
 {
 	G4cout << "MyPrimaryGenerator::MyPrimaryGenerator" << G4endl;
 	fParticleGun = new G4ParticleGun();
+
+	// default particle kinematic
+	G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
+	G4ParticleDefinition *particle = particleTable->FindParticle("geantino");
+	fParticleGun->SetParticleDefinition(particle);
+	fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,1.));
+	fParticleGun->SetParticlePosition(G4ThreeVector(0.,0.,0.));
+	fParticleGun->SetParticleEnergy(0. * eV);
 }
 
 MyPrimaryGenerator::~MyPrimaryGenerator()
@@ -13,5 +21,20 @@ MyPrimaryGenerator::~MyPrimaryGenerator()
 
 void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
 {
+	G4ParticleDefinition* particle = fParticleGun->GetParticleDefinition();
+	
+	if(particle == G4Geantino::Geantino())
+	{
+		G4int Z = 63; // Eu
+		G4int A = 152;
+		
+		G4double charge = 0. * eplus;
+		G4double energy = 0. * keV;
+		
+		G4ParticleDefinition* ion = G4IonTable::GetIonTable()->GetIon(Z, A, energy);
+		fParticleGun->SetParticleDefinition(ion);
+		fParticleGun->SetParticleCharge(charge);
+	}
+
 	fParticleGun->GeneratePrimaryVertex(anEvent);
 }
