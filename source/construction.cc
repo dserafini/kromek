2ce3ad41cd53f30bc1674eeb22205ebe1e93d084
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2024-06-30 14:14:49 +0200

    Update construction.cc

--- source/construction.cc
+++ source/construction.cc
@@ -25,11 +25,18 @@ G4VPhysicalVolume* MyDetectorConstruction::Construct()
 	G4double world_half_Z  = 10*cm;
 	G4double world_half_XY = 10*cm;
 
-	G4Box* solidWorld = new G4Box("solidWorld", world_half_XY, world_half_XY, world_half_Z);
-	G4LogicalVolume* logicWorld = new G4LogicalVolume(solidWorld, materialAir, "logicWorld", 0, 0, 0, true);
+	G4VPhysicalVolume* physWorld;
 
-	G4VPhysicalVolume* physWorld = new G4PVPlacement(0, G4ThreeVector(), logicWorld, "physWorld", 0, false, 0, true);
-
+	if (materialAir)
+	{
+		G4cout << "c'è aria" << G4endl;
+		G4Box* solidWorld = new G4Box("solidWorld", world_half_XY, world_half_XY, world_half_Z);
+		G4LogicalVolume* logicWorld = new G4LogicalVolume(solidWorld, materialAir, "logicWorld", 0, 0, 0, true);
+		physWorld = new G4PVPlacement(0, G4ThreeVector(), logicWorld, "physWorld", 0, false, 0, true);
+	}
+	else
+		G4cout << "non c'è aria" << G4endl;
+	
 	return physWorld;
 }
 
