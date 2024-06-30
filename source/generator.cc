fbc41ffc8dfba6e46ab76830d4dde45b320a805a
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2024-06-30 14:22:30 +0200

    Update generator.cc

--- source/generator.cc
+++ source/generator.cc
@@ -3,10 +3,15 @@
 MyPrimaryGenerator::MyPrimaryGenerator()
 {
 	G4cout << "MyPrimaryGenerator::MyPrimaryGenerator" << G4endl;
+	fParticleGun = 0;
 }
 
 MyPrimaryGenerator::~MyPrimaryGenerator()
-{}
+{
+	delete fParticleGun;
+}
 
 void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
-{}
+{
+	fParticleGun->GeneratePrimaryVertex();
+}
