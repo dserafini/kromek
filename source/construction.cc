8848bc990b9089be6add432eaf12f29ed2f2a397
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2024-06-30 19:22:57 +0200

    Update construction.cc

--- source/construction.cc
+++ source/construction.cc
@@ -15,6 +15,19 @@ void MyDetectorConstruction::DefineMaterials()
 	G4NistManager *nist = G4NistManager::Instance();
 
 	materialAir = nist->FindOrBuildMaterial("G4_AIR");
+
+	// aluminium
+	G4Material* materialAl = new G4Material("Al", 2.699 * g/cm3, 1);
+	materialAl->AddElement(nist->FindOrBuildElement("Al"), 1);
+
+	// czt case
+	materialCztCase = (G4Material*) materialAl;
+
+	// czt crystal
+	materialCztCrystal = new G4Material("czt",  5.605 * g/cm3, 3);
+	materialCztCrystal->AddElement(nist->FindOrBuildElement("Cd"), 1);
+	materialCztCrystal->AddElement(nist->FindOrBuildElement("Zn"), 1);
+	materialCztCrystal->AddElement(nist->FindOrBuildElement("Te"), 1);
 }
 
 void MyDetectorConstruction::ConstructCzt()
@@ -25,14 +38,14 @@ void MyDetectorConstruction::ConstructCzt()
 	G4double cztWindowDistance = 9 * cm;
 	G4ThreeVector cztCasePosition = G4ThreeVector(0, 0, cztWindowDistance + cztCaseHalfZ);
 	solidCztCase = new G4Box('solidCztCase', cztCaseHalfXY, cztCaseHalfXY, cztCaseHalfZ);
-	logicCztCase = new G4LogicalVolume(solidCztCase, materialAir, 'logicCztCase', 0, 0, 0, true);
+	logicCztCase = new G4LogicalVolume(solidCztCase, materialCztCase, 'logicCztCase', 0, 0, 0, true);
 	new G4PVPlacement(0, cztCasePosition, logicCztCase, "physCztCrystal", logicWorld, false, 0, true);
 
 	// czt crystal
 	G4double cztCrystalHalfXYZ = .5 * cm;
 	G4ThreeVector cztCrystalPosition = G4ThreeVector(0, 0, -1 * cm);
 	solidCztCrystal = new G4Box('solidCztCrystal', cztCrystalHalfXYZ, cztCrystalHalfXYZ, cztCrystalHalfXYZ);
-	logicCztCrystal = new G4LogicalVolume(solidCztCrystal, materialAir, 'logicCztCrystal', 0, 0, 0, true);
+	logicCztCrystal = new G4LogicalVolume(solidCztCrystal, materialCztCrystal, 'logicCztCrystal', 0, 0, 0, true);
 	new G4PVPlacement(0, cztCrystalPosition, logicCztCrystal, "physCztCrystal", logicCztCase, false, 0, true);
 }
 
