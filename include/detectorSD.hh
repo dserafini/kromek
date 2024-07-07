b644977ff5535be24fa0955acbfd670cdb2e8f17
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2024-07-07 22:38:39 +0200

    Update detectorSD.hh

--- include/detectorSD.hh
+++ include/detectorSD.hh
@@ -15,18 +15,12 @@ public:
 	MySensitiveDetector(G4String, const G4String&);
 	~MySensitiveDetector();
 	void Initialize(G4HCofThisEvent* hitCollection) override;
-	void SetEdep(G4double de) { fEdep = de; };
-	void SetPosition(G4ThreeVector tp) { fPosition = tp; };
-	G4double GetEdep() const { return fEdep; };
-	G4ThreeVector GetPosition() const { return fPosition; };
 
 private:
 	G4bool ProcessHits(G4Step*, G4TouchableHistory*) override;
 	void EndOfEvent(G4HCofThisEvent* hitCollection) override;
 	
 	detectorHitsCollection* fHitsCollection;
-	G4double      fEdep = 0.;
-	G4ThreeVector      fPosition = G4ThreeVector();
 };
 
 #endif
